cb65a69049ec948f1b941b84ca08943690710954
2019211001000809-yangyuxiang <1794375491>
2021-03-14 22:54:51 +0800

    HW-week2 commit

---
+++ src/main/java/com/example/yangyuxiang2019211001000809/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.yangyuxiang2019211001000809;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/com/yangyuxiang/week2/demo/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.yangyuxiang.week2.demo;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloWorld extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("HelloWorld");
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+}
